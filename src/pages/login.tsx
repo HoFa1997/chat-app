23084ea7ffcbea6eda6796fb0f593738e8e3658e
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-30 07:30:17 +0330

    some test

---
+++ src/pages/login.tsx
@@ -0,0 +1,30 @@
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const LoginPage: NextPage = () => {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+
+  if (!user) {
+    return (
+      <main className={styles.main}>
+        <Auth
+          redirectTo="http://localhost:3000/"
+          supabaseClient={supabaseClient}
+        />
+      </main>
+    );
+  }
+
+  return (
+    <>
+      <button onClick={() => supabaseClient.auth.signOut()}>Sign out</button>
+      <p>user:</p>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </>
+  );
+};
+
+export default LoginPage;
