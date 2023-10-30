11fd4f1eec5ea1dc54eb83c19df1723548d926f4
Hossein Fatemi <hfatemi1997@gmail.com>
2023-10-30 12:12:59 +0330

    change to app router

---
+++ src/app/account/page.tsx
@@ -0,0 +1,14 @@
+import AccountForm from "@/components/account/account-form";
+import { Database } from "@/types/supabase";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Account() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return <AccountForm session={session} />;
+}
