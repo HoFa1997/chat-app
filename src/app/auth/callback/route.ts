00e43516118af18b794aaf3c045ca7a81f30e130
hossein <marzban98@gmail.com>
2023-12-09 01:08:39 +0330

    refactor auth

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -27,7 +27,6 @@ export async function GET(request: Request) {
         },
       },
     );
-
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
