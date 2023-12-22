1e2124c13f2c580a1f40a0b55161a8b9838f8f7d
hossein <marzban98@gmail.com>
2023-12-22 17:23:59 +0330

    refactor: middleware for redirecting to login if it does not find a user session

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,12 +8,15 @@ export async function middleware(request: NextRequest) {
     },
   });
 
+  let checkIfCookieExists = false;
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
+          checkIfCookieExists = request.cookies.get(name)?.value ? true : false;
           return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
@@ -44,7 +47,25 @@ export async function middleware(request: NextRequest) {
     },
   );
 
-  await supabase.auth.getSession();
+  // Attempt to retrieve the current session
+  const { data, error } = await supabase.auth.getSession();
 
+  // If no valid session
+  if (!data.session && checkIfCookieExists) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Continue with the original response if the session is valid
   return response;
 }
+
+// Middleware configuration
+export const config = {
+  matcher: [
+    // The regular expression below is used to specify the paths where the middleware will be applied.
+    // '/((?!api|_next/static|_next/image|favicon.ico).*)' means that the middleware will run on all paths,
+    // except those that start with 'api', '_next/static', '_next/image', or 'favicon.ico'.
+    // This effectively excludes API routes, static files, image optimization files, and favicon requests.
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
