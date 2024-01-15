49c0018baba87dfa4213aa02c2b6e0d23c76503f
hossein <marzban98@gmail.com>
2024-01-15 22:36:52 +0330

    feat: message type = notification card

--- src/components/messages/components/chatContainer/MessagesDisplay.tsx
+++ src/components/messages/components/chatContainer/MessagesDisplay.tsx
@@ -11,6 +11,19 @@ interface MessagesDisplayProps {
   isLoadingMore: boolean;
 }
 
+const isNewDay = (currentMessageDate: string, previousMessageDate: string) => {
+  return !isSameDay(parseISO(currentMessageDate), parseISO(previousMessageDate));
+};
+
+const DateChip: React.FC<{ date: string; isScrollingUp: boolean }> = ({ date, isScrollingUp }) => (
+  <div
+    className="date_chip relative z-10 my-2 flex w-full justify-center pt-2"
+    style={{ position: isScrollingUp ? "sticky" : "relative", top: isScrollingUp ? 0 : undefined }}
+  >
+    <div className="badge relative z-10 bg-base-100">{format(parseISO(date), "MMMM do, yyyy")}</div>
+  </div>
+);
+
 const NoMessagesDisplay = () => (
   <div className="flex h-dvh items-center justify-center">
     <div className="badge badge-neutral">No messages yet!</div>
@@ -20,6 +33,12 @@ const NoMessagesDisplay = () => (
 const LoadingSpinner = () => (
   <div className="flex justify-center pt-2">
     <span className="loading loading-spinner text-primary"></span>
+  </div>
+);
+
+const SystemNotifyChip: React.FC<{ message: string }> = ({ message }) => (
+  <div className="my-4 flex justify-center pb-1">
+    <div className="badge badge-secondary">{message}</div>
   </div>
 );
 
@@ -36,31 +55,24 @@ const generateMessageElements = (
     if (index === 0 || isNewDay(message.created_at, array[index - 1]?.created_at)) {
       elements.push(<DateChip key={message.created_at} date={message.created_at} isScrollingUp={isScrollingUp} />);
     }
-    elements.push(
-      <MessageCard
-        key={message.id}
-        data={message}
-        ref={index === array.length - 1 ? messagesEndRef : null}
-        toggleEmojiPicker={toggleEmojiPicker}
-        selectedEmoji={selectedEmoji}
-      />,
-    );
+
+    if (message.type === "notification") {
+      elements.push(<SystemNotifyChip key={message.id} message={message.content} />);
+    } else {
+      elements.push(
+        <MessageCard
+          key={message.id}
+          data={message}
+          ref={index === array.length - 1 ? messagesEndRef : null}
+          toggleEmojiPicker={toggleEmojiPicker}
+          selectedEmoji={selectedEmoji}
+        />,
+      );
+    }
+
     return elements;
   });
 };
-
-const isNewDay = (currentMessageDate: string, previousMessageDate: string) => {
-  return !isSameDay(parseISO(currentMessageDate), parseISO(previousMessageDate));
-};
-
-const DateChip: React.FC<{ date: string; isScrollingUp: boolean }> = ({ date, isScrollingUp }) => (
-  <div
-    className="date_chip z-10 relative my-2 flex w-full justify-center pt-2"
-    style={{ position: isScrollingUp ? "sticky" : "relative", top: isScrollingUp ? 0 : undefined }}
-  >
-    <div className="badge relative z-10 bg-base-100">{format(parseISO(date), "MMMM do, yyyy")}</div>
-  </div>
-);
 
 export const MessagesDisplay: React.FC<MessagesDisplayProps> = ({
   messageContainerRef,
