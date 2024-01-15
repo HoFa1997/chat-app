d27e9b4682daebfdfebb48d2fc42baec9ad30d01
hossein <marzban98@gmail.com>
2024-01-15 19:00:14 +0330

    feat: tooltip for emoji

--- src/components/messages/components/chatContainer/MessageContent.tsx
+++ src/components/messages/components/chatContainer/MessageContent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useMemo } from "react";
 import DOMPurify from "dompurify";
 import { isOnlyEmoji, splitEmojis } from "@/shared";
+import { getEmojiDataFromNative } from "emoji-mart";
 interface MessageContentProps {
   data: {
     content: string;
@@ -14,22 +15,44 @@ const MessageContent: React.FC<MessageContentProps> = ({ data }) => {
   }, [data.html, data.content]);
 
   const [htmlContent, setHtmlContent] = useState(sanitizedHtml);
+  const [emojiTitles, setEmojiTitles] = useState<Record<number, string>>({});
 
   useEffect(() => {
     setHtmlContent(sanitizedHtml);
   }, [sanitizedHtml]);
 
-  // Check if the content is only emoji outside of JSX for readability.
   const contentIsOnlyEmoji = isOnlyEmoji(data.content);
+
+  useEffect(() => {
+    const loadEmojiTitles = async () => {
+      const titles: Record<number, string> = {};
+      const emojis = splitEmojis(data.content);
+      if (emojis) {
+        for (let i = 0; i < emojis.length; i++) {
+          const emojiData = await getEmojiDataFromNative(emojis[i]);
+          titles[i] = emojiData.name;
+        }
+      }
+      setEmojiTitles(titles);
+    };
+
+    if (contentIsOnlyEmoji) loadEmojiTitles();
+  }, [data.content, contentIsOnlyEmoji]);
+
+  // Check if the content is only emoji outside of JSX for readability.
 
   return (
     <div className="flex flex-col">
       {contentIsOnlyEmoji ? (
         <div>
           {splitEmojis(data.content)?.map((emoji: string, index: number) => (
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            <em-emoji key={index} native={emoji} size="4rem"></em-emoji>
+            <div key={index} className="tooltip tooltip-bottom" data-tip={emojiTitles[index]}>
+              {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                <em-emoji native={emoji} size="4rem"></em-emoji>
+              }
+            </div>
           ))}
         </div>
       ) : (
