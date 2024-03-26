e3f3ec8ef03bf9e8c481e78232efbba372d058b3
hossein <marzban98@gmail.com>
2024-03-27 00:51:46 +0330

    fix: number is not emoji

--- packages/web/src/shared/utils/emojis.ts
+++ packages/web/src/shared/utils/emojis.ts
@@ -1,4 +1,8 @@
 export const isOnlyEmoji = (str: string): boolean => {
+  // Check if the string starts with a number
+  if (/^\d/.test(str)) {
+    return false; // Return false if the string starts with a number
+  }
   // This regex pattern aims to match strings that consist solely of emojis,
   // including those with skin tone modifiers, zero-width joiners, etc.,
   // but exclude numbers and other non-emoji characters.
